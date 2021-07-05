f5947e48b5a6681d775b344d10f9bbf9c7d36bb4
Alimov Islom <csgobender@gmail.com>
2021-07-05 17:58:05 +0500

    project init

---
+++ CryptoMe/app/SceneDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  SceneDelegate.swift
+//  CryptoMe
+//
+//  Created by Alimov Islom on 05/07/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScane = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScane)
+        window?.rootViewController = MainController()
+        window?.makeKeyAndVisible()
+        
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+
+
+}
+
